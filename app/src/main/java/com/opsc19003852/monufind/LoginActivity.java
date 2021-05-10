36ce0e3112d4088960664060a412ebc15a6bc534
tireshredder <kieranharris209@gmail.com>
2021-05-10 17:33:38 +0200

    Added LoginActivity
    login activity added to project

---
+++ app/src/main/java/com/opsc19003852/monufind/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.opsc19003852.monufind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
