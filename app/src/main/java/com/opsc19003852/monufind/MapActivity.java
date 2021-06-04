eef502b7b8e09b7daa7f441a080a1c6232810bf1
j3lli <garth.belkum@gmail.com>
2021-06-04 18:29:10 +0200

    Search functionality and centering the map to the user (who is now marked with a pin too)

--- app/src/main/java/com/opsc19003852/monufind/MapActivity.java
+++ app/src/main/java/com/opsc19003852/monufind/MapActivity.java
@@ -1,9 +1,15 @@
 package com.opsc19003852.monufind;
 
 import android.Manifest;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -12,9 +18,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -25,6 +37,18 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         Toast.makeText(this, "map is ready!", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onMapReady: map is ready");
         gMap = googleMap;
+
+        if (permGranted) {
+            getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            gMap.setMyLocationEnabled(true);
+            gMap.getUiSettings().setMyLocationButtonEnabled(false);
+        }
     }
 
     private static final String TAG = "MapActivity";
@@ -33,6 +57,10 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 6969;
+    private static final float DEFAULT_ZOOM = 15f;
+
+    //widgets
+    private EditText etSearchText;
 
     //permissions flag
     private boolean permGranted = false;
@@ -40,12 +68,73 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     //map variable
     private GoogleMap gMap;
 
+    //location provider
+    private FusedLocationProviderClient fClient;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        etSearchText = (EditText) findViewById(R.id.input_search);
 
         getLocationPermission();
+    }
+
+    //initialization method
+    private void init(){
+        Log.d(TAG,"init: initializing");
+
+        etSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || event.getAction() == event.ACTION_DOWN
+                        || event.getAction() == event.KEYCODE_ENTER){
+
+                }
+                return false;
+            }
+        });
+    }
+
+    //getting the devices location
+    private void getDeviceLocation(){
+        Log.d(TAG, "getDeviceLocation: getting the devices current location");
+
+        fClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            if(permGranted) {
+                Task location = fClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "onComplete: found location!");
+                            Location currentLocation = (Location) task.getResult();
+
+                            moveCamera(new LatLng(currentLocation.getLatitude(),
+                                    currentLocation.getLongitude()), DEFAULT_ZOOM);
+
+                        }else{
+                            Log.d(TAG, "onComplete: current location is null");
+                            Toast.makeText(MapActivity.this, "unable to get current location!",
+                                            Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }catch(SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage());
+        }
+    }
+
+    //method to move the camera (view window of the map)
+    private void moveCamera(LatLng latlng, float zoom) {
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " +latlng.latitude +
+                        ", lng: " + latlng.longitude);
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoom));
     }
 
     //initializing the map
@@ -70,6 +159,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
                     COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
                 permGranted = true;
+                initMap();
             }else{
                 ActivityCompat.requestPermissions(this,
                         permissions, LOCATION_PERMISSION_REQUEST_CODE);
@@ -83,16 +173,18 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onRequestPermissionsResult(int requestCode,
             @NonNull String[] permissions, @NonNull  int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
         Log.d(TAG, "onRequestPermissionsResult: has been called");
         permGranted = false;
 
-        switch(requestCode){
+        switch (requestCode) {
             case LOCATION_PERMISSION_REQUEST_CODE: {
                 //if the first permission has been granted
-                if(grantResults.length > 0){
+                if (grantResults.length > 0) {
                     //looping through all of the grant results
-                    for(int i = 0; i < grantResults.length; i++) {
-                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
                             permGranted = false;
                             Log.d(TAG, "onRequestPermissionsResult: permissions were not granted");
                             return;
