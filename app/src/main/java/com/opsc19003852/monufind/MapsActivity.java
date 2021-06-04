18346d87f23b4a810a8590710f35f3a262b908c9
tireshredder <kieranharris209@gmail.com>
2021-06-04 18:38:00 +0200

    Map perm .3

--- app/src/main/java/com/opsc19003852/monufind/MapsActivity.java
+++ app/src/main/java/com/opsc19003852/monufind/MapsActivity.java
@@ -21,8 +21,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity {
 
+
     private GoogleMap mMap;
-    private static final String tag="MapActivity";
+    private static final String TAG ="MapActivity";
 
     private static final String FINE_LOCATION= Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION= Manifest.permission.ACCESS_COARSE_LOCATION;
@@ -37,22 +38,11 @@ public class MapsActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //Obtain the SupportMapFragment and get notified when the map is ready to be used.
 
         getLocationPermission();
 
     }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-
 
     private void initMap(){
         Log.d(TAG, "initMap: initializing map");
