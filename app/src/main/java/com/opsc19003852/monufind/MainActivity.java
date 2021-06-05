d543b26d87ffa65279fc214487207db8b69c7a07
Nebbz <euanmy@gmail.com>
2021-06-05 15:53:43 +0200

    Database added and login and registration working. No feature to add Distance type or landmark prefernece yet. will get to that when the powers back

--- app/src/main/java/com/opsc19003852/monufind/MainActivity.java
+++ app/src/main/java/com/opsc19003852/monufind/MainActivity.java
@@ -2,21 +2,77 @@ package com.opsc19003852.monufind;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText mUsername, mPassword;
+    Button mLoginbtn;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPassword=findViewById(R.id.edtPassword);
+        mUsername=findViewById(R.id.edtUsername);
+        mPassword=findViewById(R.id.edtPassword);
+        mLoginbtn=findViewById(R.id.btnRegister);
+        fAuth=FirebaseAuth.getInstance();
         TextView register = (TextView)findViewById(R.id.lnkRegister);
         register.setMovementMethod(LinkMovementMethod.getInstance());
+
+        mLoginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username =mUsername.getText().toString().trim();
+                String password=mPassword.getText().toString().trim();
+                if (TextUtils.isEmpty(username)) {
+                    mUsername.setError("Username is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is required");
+                    return;
+                }
+                if (password.length() < 6) {
+                    mPassword.setError("Password Length must be greater than 6");
+                    return;
+                }
+
+                //Authenticate USer
+                fAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Error!!!"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            }
+
+        });
         register.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
--- app/src/main/java/com/opsc19003852/monufind/RegistrationActivity.java
+++ app/src/main/java/com/opsc19003852/monufind/RegistrationActivity.java
@@ -2,19 +2,107 @@ package com.opsc19003852.monufind;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegistrationActivity extends AppCompatActivity {
+    public static final String TAG = "Tag";
+    EditText mFullname, mPassword, mUsername;
+
+    Button RegisterBtn;
+    FirebaseAuth fAuth;
+    final FirebaseDatabase fbase = FirebaseDatabase.getInstance();
+    DatabaseReference ref = fbase.getReference("server/saving-data/fireblog");
+    String userID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        mFullname=findViewById(R.id.edtFullName);
+        mPassword=findViewById(R.id.edtPassword);
+        mUsername=findViewById(R.id.edtUsername);
+        RegisterBtn=findViewById(R.id.btnRegister);
+        fAuth=FirebaseAuth.getInstance();
         TextView login = (TextView)findViewById(R.id.lnkLogin);
         login.setMovementMethod(LinkMovementMethod.getInstance());
+        if(fAuth.getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        RegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = mUsername.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                final String fullName = mFullname.getText().toString();
+                if (TextUtils.isEmpty(username)) {
+                    mUsername.setError("Username is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is required");
+                    return;
+                }
+                if (password.length() < 6) {
+                    mPassword.setError("Password Length must be greater than 6");
+                    return;
+                }
+
+
+
+                fAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegistrationActivity.this, "User Created", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DatabaseReference dr =  ref.child("users");
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("fname",fullName);
+                            user.put("username",username);
+                           
+
+                            dr.setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "OnSuccess: user profilepic is created for "+userID);
+                                }
+                            });
+
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                        else{
+                            Toast.makeText(RegistrationActivity.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+            }
+
+        });
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
